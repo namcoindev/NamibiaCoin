e55307f9b45e16070c0506faff7e0266c3f3ea7e
namcoindev <69799859+namcoindev@users.noreply.github.com>
2020-08-19 19:42:21 +0200

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -7,18 +7,29 @@
 #include <string>
 
 const std::string windowsAsciiArt = 
- "\n     __                _ _     _           ___      _  \n"     
- " /\ \ \__ _ _ __ ___ (_) |__ (_) __ _    / __\___ (_)_ __  \n"
- "/  \/ / _` | '_ ` _ \| | '_ \| |/ _` |  / /  / _ \| | '_ \ \n"
-"/ /\  / (_| | | | | | | | |_) | | (_| | / /__| (_) | | | | |\n"
-"\_\ \/ \__,_|_| |_| |_|_|_.__/|_|\__,_| \____/\___/|_|_| |_|\n";
+ "\n
+//  ooooo      ooo                               .oooooo.              o8o                                                \n"
+//  `888b.     `8'                              d8P'  `Y8b             `"'                                                \n"
+//   8 `88b.    8   .oooo.   ooo. .oo.  .oo.   888           .ooooo.  oooo  ooo. .oo.        .ooooo.  oooo d8b  .oooooooo \n"
+//   8   `88b.  8  `P  )88b  `888P"Y88bP"Y88b  888          d88' `88b `888  `888P"Y88b      d88' `88b `888""8P 888' `88b  \n"
+//   8     `88b.8   .oP"888   888   888   888  888          888   888  888   888   888      888   888  888     888   888  \n"
+//   8       `888  d8(  888   888   888   888  `88b    ooo  888   888  888   888   888  .o. 888   888  888     `88bod8P'  \n"
+//  o8o        `8  `Y888""8o o888o o888o o888o  `Y8bood8P'  `Y8bod8P' o888o o888o o888o Y8P `Y8bod8P' d888b    `8oooooo.  \n"
+//                                                                                                             d"     YD  \n"
+//                                                                                                             "Y88888P'  \n";
+                                                                                                                     
 
 const std::string nonWindowsAsciiArt =
- "\n     __                _ _     _           ___      _  \n"     
- " /\ \ \__ _ _ __ ___ (_) |__ (_) __ _    / __\___ (_)_ __  \n"
- "/  \/ / _` | '_ ` _ \| | '_ \| |/ _` |  / /  / _ \| | '_ \ \n"
-"/ /\  / (_| | | | | | | | |_) | | (_| | / /__| (_) | | | | |\n"
-"\_\ \/ \__,_|_| |_| |_|_|_.__/|_|\__,_| \____/\___/|_|_| |_|\n";
+ "\n
+//  ooooo      ooo                               .oooooo.              o8o                                                \n"
+//  `888b.     `8'                              d8P'  `Y8b             `"'                                                \n"
+//   8 `88b.    8   .oooo.   ooo. .oo.  .oo.   888           .ooooo.  oooo  ooo. .oo.        .ooooo.  oooo d8b  .oooooooo \n"
+//   8   `88b.  8  `P  )88b  `888P"Y88bP"Y88b  888          d88' `88b `888  `888P"Y88b      d88' `88b `888""8P 888' `88b  \n"
+//   8     `88b.8   .oP"888   888   888   888  888          888   888  888   888   888      888   888  888     888   888  \n"
+//   8       `888  d8(  888   888   888   888  `88b    ooo  888   888  888   888   888  .o. 888   888  888     `88bod8P'  \n"
+//  o8o        `8  `Y888""8o o888o o888o o888o  `Y8bood8P'  `Y8bod8P' o888o o888o o888o Y8P `Y8bod8P' d888b    `8oooooo.  \n"
+//                                                                                                             d"     YD  \n"
+//                                                                                                             "Y88888P'  \n";
 
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
